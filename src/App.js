07d318a44b314c3fceedd657deeba089d3eb28ca
mohdarshad1 <mdarshad181@gmail.com>
2021-06-29 16:13:10 +0530

    [Mohd Arshad] : Hello World Single Binding

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import { render } from '@testing-library/react';
 import React from 'react';
 
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//       <hi> Hello form Bridgelabz </hi>
+//       </div>
+//     );
+//    }
+// }
+
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      title: 'Hello From BridgeLabz'
+    }
+  }
   render() {
     return (
       <div>
-      <hi> Hello form Bridgelabz </hi>
+        <h1>{this.state.title}</h1>
       </div>
-    );
-    }
+    ); 
+  }
 }
-
 export default App;
