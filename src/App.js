bf4210d925601a7e9c2d07fa1704e6a57316e07b
mohdarshad1 <mdarshad181@gmail.com>
2021-06-30 22:09:39 +0530

    [Mohd Arshad] : Hello World Home Component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
-import './App.css';
 import React from 'react';
-import logo from './asset/BL_logo_square_jpg.jpg'
+import './App.css';
+import Home from './component/home';
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 class App extends React.Component {
-  url = 'https://www.bridgelabz.com/'
-  constructor(){
-    super()
-    this.state = {
-      userName: '',
-      nameError: ''
-    }
-  }
-
-  onClick = ($event) => {
-    console.log("save button is clicked!", $event);
-    window.open(this.url, "_blank");
-  }
-
-  onNameChange = (event) => {
-    console.log("value is", event.target.value);
-    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-    this.setState({userName: event.target.value})
-    if(nameRegex.test(event.target.value)){
-      this.setState({nameError: ''})
-    }else{
-      this.setState({nameError: 'Name is Incorrect'})
-    }
-
-  }
 
   render() {
     return (
-      <div>
-      <div>
-        <h1>Hello {this.state.userName} from Bridgelabz</h1>
-        <img src={logo} onClick={this.onClick} 
-        alt="The Bridgelabz logo : a Bridge to Employment through lab works" />
-      </div>
-      <div>
-        <input class="inputtext" onChange={this.onNameChange} /> 
-        <span className="error-output">{this.state.nameError}</span>
-      </div>
-      <div>
-      <p>At BridgeLabz, we're a techie community of</p>
-      <ul>
-      <li>technologists</li>
-      <li>thinkers</li>
-      <li>builders</li>
-      </ul>
-      <p>Working together to keep the Tech Employability of Engineers alive and accessible,
-      so Tech Companies worldwide can get contributors and creators for Technology Solutions.
-      </p>
-
-      <p>To know about us, visit <a href="https://www.bridgelabz.com/">BridgeLabz</a>
-      to learn evem more about our mission i.e. <strong> Employability to all</strong>.</p>
-      </div>
+      <div className="app-main">
+        <Router>
+          <Switch>
+            <Route path="">
+            <Home />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
