921153b04daad075e08d8474a272e9c446ca1dcb
mohdarshad1 <mdarshad181@gmail.com>
2021-06-30 20:20:33 +0530

    [Mohd Arshad] : Hello World Event Binding

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import './App.css';
 import React from 'react';
 import logo from './asset/BL_logo_square_jpg.jpg'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state={
-      title: "Hello From BrigdeLabz"
+class App extends React.Component{ 
+  url = 'https://www.bridgelabz.com/'
+  constructor(){ 
+    super() 
+    this.state={ 
+      title:'Hello From BridgeLabz' 
+    } 
+  } 
+
+  //onClick function
+  onClick = ($event) =>{
+    console.log("save button is clicked",$event)
+    window.open(this.url, "_blank")
+  }
+
+  render(){
+    return( 
+      <div> 
+        <h1>{this.state.title}</h1> 
+        <img src={logo} onClick={this.onClick}
+              alt="The BridgeLabz Logo: a Bridge to Employeement through lab works" />
+      </div> 
+      );
     }
-  }
-  render() {
-    return (
-      <div>
-      <h1>{this.state.title}</h1>
-      <img src={logo} alt="The BridgeLabz Logo" />
-      </div>
-    );
-  }
 }
 export default App;
